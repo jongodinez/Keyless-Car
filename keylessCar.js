7d594a8dd0a9f785fe3e1d74d9521d73df6b24c4
jongodinez <43837087+jongodinez@users.noreply.github.com>
2018-10-15 23:54:53 -0700

    Update keylessCar.js
    Layered four functions to create a mean car that you can only drive if you're 16+

--- keylessCar.js
+++ keylessCar.js
@@ -1,24 +1,32 @@
-// Make a keyless car!
-// This car will only let you drive if you are over 18. Make it do the following:
+//Layered four functions to create a mean car that you can only drive if you're 16+
 
-
-//using prompt() and alert(), ask a user for their age.
-// IF they say they are below 18, respond with:
-// "Sorry, you are too young to drive this car. Powering off"
-
-// IF they say they are 18, respond with:
-// "Congratulations on your first year of driving. Enjoy the ride!"
-
-// IF they say they are over 18, respond with:
-// "Powering On. Enjoy the ride!"
-
-alert("Car has turned on.");
-
-var age = prompt("Whoa whoa whoa, how old are you?");
-if (age > 16) {
-    alert("I hope you're driving to your job you lazy bum.");
-} else if (age < 16){
-    alert("Sorry, you are too young to drive this car. Powering off");
-} else if (age = 16) {
-    alert("Congratulations on your first year of driving. Enjoy the ride!");
+function turnTheKey () {
+    key = prompt("Turn the car on by turning the key. (Type: 'turn key')");
+    if (key === "turn key") {
+        carTurnsOn ();
+    }
 }
+function carTurnsOn () {
+    var user = prompt("Car has turned on. Would you like to drive?");
+    if (user === "yes") {
+        checkDriverAge ();
+    } else if (user === "no") {
+       smartAss ();
+    }
+}
+function smartAss() {
+    prompt("Why are you here.");
+    alert("You think I actually care.");
+}
+function checkDriverAge () {
+    var age = prompt("Whoa whoa whoa, how old are you?");
+    if(age > 16) {
+        alert("You're old enough to drive, don't get a DUI!");
+    } else if (age === 16) {
+        alert("You've been driving less than a year, be careful now.");
+    } else if (age < 16) {
+        alert("Where are your parents you should not be driving!");
+    }
+}
+turnTheKey();
+
